1395ec31cd42e9db2b1855d569a0c34ce350ed65
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-03-19 16:23:48 -0600

    Añadiendo archivos de texto

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
-
+/**
+ * @author Marcos Gutierrez				17909
+ * @author Raul Monzon					17014
+ * Clase principal
+ */
 
 
 public class Main {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
